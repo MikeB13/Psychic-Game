a3eb2ea06d0b9c2b0f6ded38fde59c08fac7496f
Mike B <mike.babayev@gmail.com>
2019-06-03 22:20:47 -0400

    final fix

--- game.js
+++ game.js
@@ -5,7 +5,8 @@ var wins = 0;
 
 // Create a var for computer to generate a random letter
 
-var computerGuess = String.fromCharCode(Math.random() * 26);
+var computerGuess =
+    String.fromCharCode(Math.random(Math.random() * 26) + 97);
 
 // Log the computers guess
 console.log(computerGuess);
@@ -13,9 +14,10 @@ console.log(computerGuess);
 // create function to capture the users input in lower case
 
 document.onkeydown = function(event) {
-    var keyPress = (String.fromCharCode(event.keyCode)).toLowerCase();
+    var keyPress =
+        (String.fromCharCode(event.keyCode)).toLowerCase();
 
-    document.getElementById('guess').innerHTML = keyPress;
+    // document.getElementById('guess').innerHTML = "keyPress";
     addLetter(keyPress);
 }
 
@@ -44,6 +46,53 @@ function addLetter(userKeypress) {
 
 // create function to show the guessed letters
 
+function showLettersGuessed() {
+    var usedString = lettersGuessed.join(", ");
+    document.getElementById("playerGuesses").innerHTML = usedString;
+}
+
+
+function guessMatch(character) {
+    console.log(character);
+    console.log(computerGuess);
+
+    if (character === computerGuess) {
+        alert("You Won!!");
+        wins = wins + 1;
+        showWins();
+    } else if (guessesLeft === 0) {
+        alert("Too Bad!!! Try again");
+        resetVariables();
+    } else {
+        guessesLeft = guessesLeft - 1;
+        showRemainingGuesses();
+    }
+}
+
+// create a function to show the wins
+
+function showWins() {
+    document.getElementById("numWins").innerHTML = wins;
+}
+
+// create a funtion to show remaining guesses
+function showRemainingGuesses() {
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+}
+
+// reset variables
+
+function resetVariables() {
+    lettersGuessed = [];
+    guessesLeft = 10;
+}
+
+function start() {
+    showRemainingGuesses();
+    showWins;
+}
+
+// startGame();
 
 
 
@@ -53,8 +102,6 @@ function addLetter(userKeypress) {
 
 
 
-
-// create function to show wins
 
 
 // create function to show guesses remaining
